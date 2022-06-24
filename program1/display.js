b7b9638e7fe2895e6c2a536b9d30de8d624251aa
GUC <engineer@guc.com>
2022-06-24 01:07:02 +0100

    program1

---
+++ program1/display.js
@@ -0,0 +1,7 @@
+let my_name = "lawan mustapha Aji";
+let hieght = "1.7";
+let country = "nigeria";
+// console.log(my_name);
+window.alert(
+  "name: " + my_name + "\nhieght:" + hieght + "\ncountry:" + country
+);
